9b891b108d61de4e1b966b05e04480934a9d17ad
Jonathan Henrique <silva.henriquejonas@gmail.com>
2023-11-10 03:59:23 -0300

    14  com correcoes sem cors e sem security talvez url do deploy ou https://cuidar.up.railway.app ou https://genaws.up.railway.app

--- src/main/java/com/jonathan/genaws/configuration/CorsConfig.java
+++ src/main/java/com/jonathan/genaws/configuration/CorsConfig.java
@@ -1,33 +1,33 @@
-package com.jonathan.genaws.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-
-        // Permitir qualquer origem
-        config.addAllowedOrigin("*");
-
-        // Permitir todos os métodos (GET, POST, PUT, DELETE, etc.)
-        config.addAllowedMethod("*");
-
-        // Permitir todos os headers
-        config.addAllowedHeader("*");
-
-        // Permitir credenciais (por exemplo, para suporte a cookies)
-        config.setAllowCredentials(true);
-
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
-    }
-}
+//package com.jonathan.genaws.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//
+//        // Permitir qualquer origem
+//        config.addAllowedOrigin("*");
+//
+//        // Permitir todos os métodos (GET, POST, PUT, DELETE, etc.)
+//        config.addAllowedMethod("*");
+//
+//        // Permitir todos os headers
+//        config.addAllowedHeader("*");
+//
+//        // Permitir credenciais (por exemplo, para suporte a cookies)
+//        config.setAllowCredentials(true);
+//
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsFilter(source);
+//    }
+//}
--- src/main/java/com/jonathan/genaws/configuration/SecurityConfig.java
+++ src/main/java/com/jonathan/genaws/configuration/SecurityConfig.java
@@ -1,35 +1,35 @@
-package com.jonathan.genaws.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SecurityConfig {
-
-    private final String[] AUTH_LIST_SWAGGER = {
-            "/swagger-ui/**", "/v3/api-docs/**", "/configuration/ui",
-            "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**",
-            "/swagger-resources/configuration/ui", "/swagge‌​r-ui.html", "/swagger-resources/configuration/security"
-    };
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        // Habilitando o h2 console no navegador
-        http.headers().disable();
-
-        http.authorizeRequests(authorizeRequests -> authorizeRequests
-                .antMatchers(AUTH_LIST_SWAGGER).permitAll()
-                .anyRequest().permitAll()
-        );
-
-        http.csrf().disable() // Desativa a proteção CSRF (pode ser necessário para aplicativos que usam apenas APIs)
-                .cors().disable() // Desativa o controle CORS
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        return http.build();
-    }
-}
+//package com.jonathan.genaws.configuration;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    private final String[] AUTH_LIST_SWAGGER = {
+//            "/swagger-ui/**", "/v3/api-docs/**", "/configuration/ui",
+//            "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**",
+//            "/swagger-resources/configuration/ui", "/swagge‌​r-ui.html", "/swagger-resources/configuration/security"
+//    };
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        // Habilitando o h2 console no navegador
+//        http.headers().disable();
+//
+//        http.authorizeRequests(authorizeRequests -> authorizeRequests
+//                .antMatchers(AUTH_LIST_SWAGGER).permitAll()
+//                .anyRequest().permitAll()
+//        );
+//
+//        http.csrf().disable() // Desativa a proteção CSRF (pode ser necessário para aplicativos que usam apenas APIs)
+//                .cors().disable() // Desativa o controle CORS
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//        return http.build();
+//    }
+//}
